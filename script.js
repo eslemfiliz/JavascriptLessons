30de8bdb51c2d95e9b4173a8c9d9e9ba1b5d8376
eslemfiliz <138863865+eslemfiliz@users.noreply.github.com>
2024-08-31 14:11:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+//Diziler
+// var phones=[
+//     "Apple",
+//     "Honor",
+//     "Huawei",
+//     "Samsung",
+//     "Vestel"
+// ];
+// console.log(phones);
+// console.log(phones[2]);
+// var telefonMarkasi="Telefon Markam:"+phones[0];
+// console.log(telefonMarkasi);
+
+// var phones=[
+//     "Apple",
+//     "Huawei",
+//     "Samsung",
+// ];
+// var phone={phoneName:'Apple',phoneColor:"Blue",phoneStockNumber:61,phoneSockState:true};
+// console.log(phone);
+
+// var phones=[
+//     "Apple",
+//     "Huawei",
+//     "Samsung",
+//     "Vestel"
+// ];
+// console.log("Magazamızda"+phones.length+ "adet farklı telefon bulunmaktadır");
+
+/*son elemanı bulmamızı sağlar*/ 
+// console.log(phones[phones.length-1]);
+/*eleman ekleme,listenin sonuna ekler*/
+// phones.push("Oppo");
+// console.log(phones);
+
+
+// var numbers= new Array(40,50,100);
+// console.log(numbers); 
+
+// var numbers= new Array(40);
+// console.log(numbers); 
+
+
+// var phones=[
+//     "Apple",
+//     "Huawei",
+//     "Samsung",
+//     "Vestel"
+// ];
+// var cikarilanEleman=phones.pop();
+// var eklendi=phones.push("Tesla");
+// console.log(phones);
+// console.log("Cikarilan eleman:"+cikarilanEleman,"Magazadaki farklı telefon değeri:"+eklendi);
+
+// var phones=[
+//     "Apple",
+//     "Huawei",
+//     "Samsung",
+//     "Vestel"
+// ];
+// phones.splice(2,0,"Oppo","LG");
+// console.log(phones);
+// var diziBir=["A","B","C"];
+// var diziIki=[1,2,3,4,5];
+// var yeniDizi=diziBir.concat(diziIki);
+// console.log(yeniDizi);
+
+// var okulAracGerecleri=["Öğrenci","Kalem","Silgi","Öğretmen","Çanta","Suluk","Defter","Önlük","Çelik"];
+// okulAracGerecleri.sort();
+// okulAracGerecleri.reverse();
+// console.log(okulAracGerecleri);
